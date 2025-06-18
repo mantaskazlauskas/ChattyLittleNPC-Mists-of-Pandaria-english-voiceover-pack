f3fe6cf22fbe5e2397956a191c0d624f50e46ab2
Tuubby <mmantas.kazlauskas@gmail.com>
2025-06-18 11:36:55 +0300

    Init Commit

---
+++ Main.lua
@@ -0,0 +1,9 @@
+---@class ChattyLittleNpc_Mists_of_Pandaria
+local ChattyLittleNpc_Mists_of_Pandaria_voiceovers = LibStub("AceAddon-3.0"):NewAddon("ChattyLittleNpc_Mists_of_Pandaria_voiceovers")
+
+ChattyLittleNpc_Mists_of_Pandaria_voiceovers.Metadata = {
+    ContainsQuestVoiceovers = true,
+    ContainsGossipVoiceovers = false,
+    ContainsGameObjectVoiceovers = false,
+    ContainsItemVoiceovers = false
+}
