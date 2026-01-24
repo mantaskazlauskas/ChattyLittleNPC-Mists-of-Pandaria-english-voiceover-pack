bd28f96453a544ee9dd27bad9994411148096548
Mantas Kazlauskas <mmantas.kazlauskas@gmail.com>
2026-01-24 10:10:00 +0200

    generated the voiceovers with new TTS model.

--- ChattyLittleNpc_Mists_of_Pandaria_voiceovers/Main.lua
+++ ChattyLittleNpc_Mists_of_Pandaria_voiceovers/Main.lua
@@ -1,4 +1,4 @@
----@class ChattyLittleNpc_Mists_of_Pandaria
+---@class ChattyLittleNpc_Mists_of_Pandaria_voiceovers
 local ChattyLittleNpc_Mists_of_Pandaria_voiceovers = {}
 _G["ChattyLittleNpc_Mists_of_Pandaria_voiceovers"] = ChattyLittleNpc_Mists_of_Pandaria_voiceovers
 
